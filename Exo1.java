bf1baec0419e90455bc52d8fde4c91cceba8aa7a
nerojeni <136816309+nerojeni@users.noreply.github.com>
2024-01-31 11:13:03 +0100

    s

--- Exo1.java
+++ Exo1.java
@@ -1,7 +1,9 @@
 public public class Exo1 implements Ttfuntion(){
 
-    public readCsv(String filePath, int Ra ){
+    public readCsv(String filePath, int RandomNumber){
 
+        String filePath = "EXPORTCSV.csv";
+        
 
 
     }
